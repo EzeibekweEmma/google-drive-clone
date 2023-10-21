c9430d004d264f3fc4d82fcae94a92c3ffb936cd
Ezeibekwe Emmanuel <ezeibekweemma@gmail.com>
2023-10-21 03:17:13 +0100

    feat: Add functions for file management
    This commit introduces functions for renaming, starring, moving to trash, and deleting files in the application. These functions allow users to interact with their files and update file properties as needed.
    
    renameFile function enables renaming a file by updating its fileName property.
    starFile function allows users to toggle the isStarred property of a file.
    trashFile function permits users to move a file to the trash by setting isStarred to false and isTrashed to the desired value.
    deleteFile function provides the capability to permanently delete a file from the Firestore database.

--- src/API/Firestore.tsx
+++ src/API/Firestore.tsx
@@ -1,5 +1,11 @@
 import { database } from "@/firebaseConfig";
-import { collection, addDoc } from "firebase/firestore";
+import {
+  collection,
+  addDoc,
+  doc,
+  updateDoc,
+  deleteDoc,
+} from "firebase/firestore";
 
 let files = collection(database, "files");
 
@@ -14,6 +20,8 @@ export const addFiles = (
       fileLink: fileLink,
       fileName: fileName,
       isFolder: false,
+      isStarred: false,
+      isTrashed: false,
       folderId: folderId,
       userEmail: userEmail,
     });
@@ -29,5 +37,48 @@ export const addFolder = (payload: payloadProps) => {
     });
   } catch (err) {
     console.error(err);
+  }
+};
+
+export const renameFile = async (fileId: string, fileName: string) => {
+  const fileRef = doc(files, fileId);
+  try {
+    await updateDoc(fileRef, {
+      fileName: fileName,
+    });
+  } catch (error) {
+    console.error("Error updating file properties: ", error);
+  }
+};
+
+export const starFile = async (fileId: string, isStarred: boolean) => {
+  const fileRef = doc(files, fileId);
+  try {
+    await updateDoc(fileRef, {
+      isStarred: isStarred,
+    });
+  } catch (error) {
+    console.error("Error updating file properties: ", error);
+  }
+};
+
+export const trashFile = async (fileId: string, isTrashed: boolean) => {
+  const fileRef = doc(files, fileId);
+  try {
+    await updateDoc(fileRef, {
+      isStarred: false,
+      isTrashed: isTrashed,
+    });
+  } catch (error) {
+    console.error("Error updating file properties: ", error);
+  }
+};
+
+export const deleteFile = async (fileId: string) => {
+  const fileRef = doc(files, fileId);
+  try {
+    await deleteDoc(fileRef);
+  } catch (error) {
+    console.error("Error deleting file: ", error);
   }
 };
